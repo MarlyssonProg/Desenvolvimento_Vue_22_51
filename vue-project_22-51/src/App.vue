41a38c6c4de8618b951eb53e369c2a24eee5a00e
MarlyssonProg <designprogthe@gmail.com>
2024-01-31 07:33:46 -0300

    Part2 Desenvolvimento Vue

---
+++ vue-project_22-51/src/App.vue
@@ -0,0 +1,10 @@
+<!--Importações dos componentes-->
+<script setup>
+  import Exemplo14 from './componentes/Exemplo14Bootstrap.vue';
+</script>
+
+<!--HTML---->
+<template>
+  <Exemplo14></Exemplo14>
+</template>
+
---
+++ vue-project_22-51/src/componentes/Exemplo14Bootstrap.vue
@@ -0,0 +1,10 @@
+<!--SCRIPT-->
+<script setup>
+//Importações do Bootstrap 
+</script>
+
+<!--HTML-->
+
+<template>
+    <h1>Utilizando o Bootstrap</h1>
+</template>
